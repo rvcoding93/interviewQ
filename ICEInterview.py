7e60572b601dccefeb08bb1e997a5eb525fd2806
Ronnie <ronnievcoding@gmail.com>
2022-06-28 03:42:41 -0400

    add extra credit function, the function counts the number of lines and  words

--- ICEInterview.py
+++ ICEInterview.py
@@ -1,17 +1,21 @@
 # Please provide a sample python program that takes text file 
 # as input and produces output showing the most used words in 
 # that text in descending order.
+#-----------------XC-----------------------------------------------------
+# For extra credit show the total number of words, total 
+# number of lines, and the total number of paragraphs.
 #=====================================================================================
 
+from tabulate import tabulate
 
-file = open("interviewText.txt","r") #==========|'r' is reading mode in py|==========
+fileSource = open("interviewText.txt","r") #==========|'r' is reading mode in py|==========
 wordCount = []
 wordUses = 0
+lineCount = 0
 
 
-
-
-
-
-#================STUDY NOTES============================================================
-#
+#Extra Credit Function, Count # of lines, #of words
+Lines = fileSource.readlines()         #readlines does exactly that, from the text file referenced.
+for line in Lines:
+        lineCount += 1
+        print("Line{}: , Words:{}".format(lineCount, line.strip()))
