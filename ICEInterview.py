7693d2bd1de449ca2051f1eed982ecf02aabf173
Ronnie <ronnievcoding@gmail.com>
2022-06-28 02:28:54 -0400

    inital commit + files

---
+++ ICEInterview.py
@@ -0,0 +1,17 @@
+# Please provide a sample python program that takes text file 
+# as input and produces output showing the most used words in 
+# that text in descending order.
+#=====================================================================================
+
+
+file = open("interviewText.txt","r") #==========|'r' is reading mode in py|==========
+wordCount = []
+wordUses = 0
+
+
+
+
+
+
+#================STUDY NOTES============================================================
+#
